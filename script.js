334c63fe148d255ceaaa4ef877bb20f648767210
Jahanzaib Hussain <m.jahanzaibhussain@gmail.com>
2016-11-10 20:12:04 +0500

    submit button works perfectly and storing data into local storage but login button don't

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 function data () {
-var formdata,name,rollnum,a;
-formdata = document.getElementById("form1");
 
+var formdata,name,rollnum;
+
+formdata = document.getElementById("form1");
 name = formdata.elements["name"].value;
- 
 rollnum = formdata.elements["rollnum"].value;
 
-var namearr = [];
-namearr.push(name);
-
+if(name.value == "" || rollnum.value == ""){
+    	alert("field cannot be blank");
+    }
+    else{
+    	localStorage.setItem(name , rollnum);
+    	alert("Your Data hass been Stored ");
+    }
 }
+
+
+/*function login(){
+
+	for(var i = 0; i < namearr.length ; i++){
+		if (rollnum.value == localStorage.getItem(name.value)) {
+			alert("Success fonud");
+		}else {
+			alert("Sign up First!");
+		}
+	}
+}*/
